9cd8602d68c303f4ae496fd12adfd13839780335
Brandon Kong <kongbrandon0@gmail.com>
2024-05-03 22:29:20 -0500

    Updated Menu class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils.logger.LoggerFactory import LoggerFactory
-
+from utils.Menu import Menu
 from utils.Printer import Printer
 
 def main ():
@@ -16,6 +16,21 @@ def main ():
 
     printer.print('Please select an option from the menu below:\n')
 
+    options = {
+        '1. Perform K-Means Clustering': lambda: printer.print('Print'),
+        '2. Perform PCA': lambda: printer.print('Print'),
+        '3. Perform t-SNE': lambda: printer.print('Print'),
+        '4. Exit': lambda: {
+            printer.print('Exiting program...'),
+            logger.log('Program ended.'),
+            exit()
+        }
+    }
+    # Print the menu
+    menu = Menu(options)
+
+    # Run the menu
+    menu.run()
 
     # Log the end of the program
     logger.log('Program ended.')
--- utils/Menu.py
+++ utils/Menu.py
@@ -6,6 +6,26 @@ from utils.logger.LoggerFactory import LoggerFactory
 type Options = Dict[str, Callable[[], None]]
 
 class Menu:
+    """
+    The Menu class provides a way to create a menu that allows the user to select an option from a list of options.
+    Ensure that the options are a dictionary where the key is the option and the value is the action to be performed.
+    The key should be a string that also describes the action to be performed.
+
+    Example:
+
+    ```python
+    options = {
+        '1. Perform K-means clustering': lambda: print('Performing K-means clustering'),
+        '2. Perform PCA': lambda: print('Performing PCA'),
+        '3. Perform t-SNE': lambda: print('Performing t-SNE'),
+    }
+
+    menu = Menu(options)
+
+    menu.run()
+    ```
+
+    """
     def __init__(self, options: Options):
         self.options = options
 
@@ -15,10 +35,29 @@ class Menu:
     def run(self):
         while True:
             self.printer.print('Please select an option from the menu below:\n')
-            for option, action in self.options.items():
-                self.printer.print(f'{option}. {action.__name__}\n')
-            choice = input()
+            
+            enumerated = enumerate(self.options)
+            
+            for index, option in enumerate(self.options):
+                self.printer.print(f'{index + 1}. {option}')
+
+            choice = input('Enter your choice: ')
+
+            # Check if the choice is a number
+            if not choice.isdigit():
+                self.printer.error('Invalid option')
+                continue
+
+            choice = int(choice) - 1
+
+            if choice < 0 or choice >= len(self.options):
+                self.printer.error('Invalid option')
+                continue
+
+            choice = list(enumerated)[choice][1]
+
             action = self.options.get(choice)
+
             if action:
                 action()
             else:
