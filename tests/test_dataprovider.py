9134e0f8345b94229f8aebf8618cfe3ed85a6b58
Brandon Kong <kongbrandon0@gmail.com>
2024-05-04 12:09:36 -0500

    test: updated test_dataprovider unittest structure

--- tests/test_dataprovider.py
+++ tests/test_dataprovider.py
@@ -6,33 +6,35 @@ class TestDataProvider(unittest.TestCase):
     def setUp(self):
         self.data_provider = DataProvider[str]()
         
-    def test_add_data(self):
+    def test_add_data_stores_data_correctly(self):
         self.data_provider.add_data('test', 'test')
         self.assertEqual(self.data_provider.get_data('test'), 'test')
 
+    def test_add_data_stores_data_correctly_with_nested_key(self):
         self.data_provider.add_data('test1/test2', 'test')
         self.assertEqual(self.data_provider.get_data('test1/test2'), 'test')
 
+    def test_add_data_stores_data_correctly_with_multiple_nested_keys(self):
         self.data_provider.add_data('test1/test3', 'test')
         self.assertEqual(self.data_provider.get_data('test1/test3'), 'test')
 
-    def test_add_data_override(self):
+    def test_add_data_overrides_existing_data(self):
         self.data_provider.add_data('test', 'test')
-        self.assertEqual(self.data_provider.get_data('test'), 'test')
-
         self.data_provider.add_data('test', 'test1')
         self.assertEqual(self.data_provider.get_data('test'), 'test1')
 
-        self.assertRaises(ValueError, self.data_provider.add_data, 'test/test2', 'test2')
+    def test_add_data_raises_error_for_invalid_key(self):
+        with self.assertRaises(ValueError) as context:
+            self.data_provider.add_data('test/test2', 'test2')
+        
 
-    def test_get_data(self):
+    def test_get_data_returns_correct_data(self):
         self.data_provider.add_data('test', 'test')
         self.assertEqual(self.data_provider.get_data('test'), 'test')
 
+    def test_get_data_returns_correct_data_with_nested_key(self):
         self.data_provider.add_data('test1/test2', 'test')
         self.assertEqual(self.data_provider.get_data('test1/test2'), 'test')
-
-        self.assertIsNone(self.data_provider.get_data('test2'))
 
         
 
