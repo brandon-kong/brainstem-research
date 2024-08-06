0cda03bd9e83298481cfd3fb61d3103aa98cc337
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2024-08-05 22:48:40 -0500

    feat: added logos on left side of navbar

--- web/src/components/navigation/navbar/index.tsx
+++ web/src/components/navigation/navbar/index.tsx
@@ -7,53 +7,118 @@ import { motion } from 'framer-motion';
 import { Role } from './components';
 
 import { NavbarNavigationButtons} from "@/components/navigation/navbuttons";
+import {Button} from "@/components/button";
+import {Separator} from "@/components/separator";
+import Link from "next/link";
+import Image from "next/image";
+import { P } from "@/components/typography";
+import {
+    NavigationMenu, NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuList,
+    NavigationMenuTrigger
+} from "@/components/ui/navigation-menu";
 
 export default function Navbar() {
-    const [open, setOpen] = useState(false);
 
     return (
         <div
-            className={'flex-col flex sidebar-shown:hidden relative shadow-md'}
+            className={'flex-col flex sticky top-0 w-full h-fit z-[100] bg-transparent'}
         >
-            <aside className="z-[100] bg-background flex flex-row justify-center fixed py-3 px-6 icon-only:px-6 w-full h-navbar">
-                <Role />
+            <div className={'bg-[#0E4174] w-full text-neutral-100 p-2 h-12 text-center text-xs flex items-center justify-center'}>
+                <p>
+                    Powered by
+                    <Link href={'https://www.depaul.edu'} className={'text-sm text-white font-bold hover:underline'}> DePaul University </Link>
+                    and
+                    <Link href={'https://www.rosalindfranklin.edu'} className={'text-sm text-white font-bold hover:underline'}> Rosalind Franklin University</Link>
+                </p>
+            </div>
 
-                <div className={'h-full items-center justify-center flex'}>
-                    <button
-                        onClick={() => setOpen(!open)}
-                        className={
-                            'group cursor-pointer flex items-center justify-center rounded-xl w-10 h-10 p-1 bg-transparent transition-colors duration-300'
-                        }
+            <aside className="flex flex-row shadow justify-between items-center px-6 py-4 w-full min-h-16">
+                <div
+                className={'flex items-center gap-2 text-black'}
+                >
+                    <Link
+                        target={'_blank'}
+                        href={'https://www.depaul.edu'}>
+                        <Image
+                            src={'/depaul.png'}
+                            alt={'logo'}
+                            className={'w-10 h-10'}
+                            width={40}
+                            height={40}
+                        />
+                    </Link>
+                     <P
+                    className={'text-xl font-light'}
                     >
-                        <Menu className="w-6 h-6 stroke-accent-foreground" />
-                    </button>
+                         x
+                        </P>
+                     <Link
+                        target={'_blank'}
+                        href={'https://www.rosalindfranklin.edu'}>
+                        <Image
+                            src={'/rosalind-franklin.png'}
+                            alt={'logo'}
+                            className={'w-10 h-10'}
+                            width={40}
+                            height={40}
+                        />
+                     </Link>
+                    <Separator
+                        orientation={'vertical'}
+                        className={'h-6 bg-neutral-200 border border-neutral-200'}
+                    />
+                    <div>
+                        <NavigationMenu>
+                            <NavigationMenuList>
+                                <NavigationMenuItem>
+                                    <NavigationMenuTrigger>
+                                        Data Pipeline
+                                    </NavigationMenuTrigger>
+                                    <NavigationMenuContent>
+                                        test
+                                    </NavigationMenuContent>
+                                </NavigationMenuItem>
+                                <NavigationMenuItem>
+                                    <NavigationMenuTrigger>
+                                        Brain Visualization
+                                    </NavigationMenuTrigger>
+                                    <NavigationMenuContent>
+                                        test
+                                    </NavigationMenuContent>
+                                </NavigationMenuItem>
+                            </NavigationMenuList>
+                        </NavigationMenu>
+                    </div>
+                </div>
+                <div className={'h-full items-center justify-center flex gap-2'}>
+                     <Button
+                        variant={"ghost"}
+                        >
+                        Data Pipeline
+                    </Button>
+                     <Button
+                        variant={"ghost"}
+                        >
+                        Brain Visualization
+                    </Button>
+
+                    <Separator
+                        orientation={'vertical'}
+                        className={'h-6 bg-neutral-200 border border-neutral-200'}
+                    />
+                    <Button
+                        variant={"ghost"}
+                        >
+                        Log in
+                    </Button>
+                    <Button
+                        >
+                        Create an account
+                    </Button>
                 </div>
             </aside>
-
-            <motion.div
-                initial={{ opacity: 0, visibility: 'hidden' }}
-                animate={{
-                    opacity: open ? 1 : 0,
-                    visibility: open ? 'visible' : 'hidden',
-                }}
-                exit={{ opacity: 0, visibility: 'hidden' }}
-                transition={{
-                    opacity: { duration: 0.2 },
-                    visibility: { delay: open ? 0 : 0.2 },
-                }}
-                className={
-                    'fixed top-0 left-0 w-full h-full bg-black/50 z-[99]'
-                }
-                onClick={() => setOpen(false)}
-            >
-                <div
-                    className={
-                        'flex-col items-center justify-center flex bg-background px-6 pb-4 pt-navbar pointer-events-auto'
-                    }
-                >
-                    <NavbarNavigationButtons />
-                </div>
-            </motion.div>
         </div>
     );
 }
